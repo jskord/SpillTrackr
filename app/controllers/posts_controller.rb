470da8af194da3e073a46df236d7a9cd1cb30c0f
justin skord <jskord@gmail.com>
2017-01-21 22:18:56 -0600

    redirect unsignedin users

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
 
   def create
+    if current_user
     post = Post.new(
       user_id: current_user.id,
       spill_id: params["spill_id"],
@@ -8,10 +9,9 @@ class PostsController < ApplicationController
       image_url: params["image_url"],
       url: params["url"],
       image: params[:image])
-    if post.save
-      redirect_to "/spills/#{post.spill.id}"
     else
-      render 'dfkndkf'
+      flash[:warning] = 'Login or Create an Account to Post'
+      redirect_to '/sign-in'
     end
   end
 end
