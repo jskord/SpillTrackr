4af80e9f6200b906f1842197feb089642e792981
justin skord <jskord@gmail.com>
2017-01-26 20:16:06 -0600

    hard coded show page map

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -112,10 +112,10 @@ class MapsController < ApplicationController
     # render json: year_month_range.to_json
   end
 
-  def show
+  def json_show
     spill_id = params[:id]
-    spill = Spill.find_by(id: spill_id)
-    render json: spill.to_json
+    @spill = Spill.find_by(id: spill_id)
+    render json: @spill.to_json
   end
 
 end
